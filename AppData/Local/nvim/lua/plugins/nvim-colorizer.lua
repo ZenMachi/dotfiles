3e604169673bdf13e0d1e6e5d6aed85a3515a26d
ZenMachi <alheta.matalarens@gmail.com>
2025-07-04 21:51:08 +0700

    Update .chezmoiignore Update AppData/Local/nvim/lazy-lock.json Update AppData/Local/nvim/lua/config/lazy.lua Update AppData/Local/nvim/lua/plugins/nvim-colorizer.lua

--- AppData/Local/nvim/lua/plugins/nvim-colorizer.lua
+++ AppData/Local/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,6 +1,11 @@
 return {
-  "catgoose/nvim-colorizer.lua",
-  event = "BufReadPre",
+  "norcalli/nvim-colorizer.lua",
   opts = { -- set to setup table
   },
+  config = function()
+    require("colorizer").setup({
+      "*", -- Highlight all files, but customize some others.
+      css = { rgb_fn = true, hsl_fn = true },
+    })
+  end,
 }
