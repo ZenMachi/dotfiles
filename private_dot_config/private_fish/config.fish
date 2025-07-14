71da99f26949593449721b84a7c026c7056c82b7
ZenMachi <alheta.matalarens@gmail.com>
2025-07-14 15:08:00 +0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,9 +6,6 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(devbox global shellenv)"
 
 # source /usr/share/nvm/init-nvm.sh
-set -gx PATH "$PATH:$HOME/.puro/bin" # Added by Puro
-set -gx PATH "$PATH:$HOME/.puro/shared/pub_cache/bin" # Added by Puro
-set -gx PATH "$PATH:$HOME/.puro/envs/default/flutter/bin" # Added by Puro
-set -gx PURO_ROOT "/home/zenmachi/.puro" # Added by Puro
+
 
 starship init fish | source
