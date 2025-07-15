7ee60bf0985464ead5a5f3adc9e81db54150e004
ZenMachi <alheta.matalarens@gmail.com>
2025-07-15 17:37:03 +0700

    Update .bash_profile Update .wezterm.lua Update .config/easyeffects/output/HB-Flat.json Update .config/nvim/lua/config/options.lua Update .config/nvim/lua/plugins/nvim-cmp.lua Update .config/fish/config.fish

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,5 +8,6 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices.
 
 config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 
 return config
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,6 +6,11 @@ eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 eval "$(devbox global shellenv)"
 
 # source /usr/share/nvm/init-nvm.sh
+set -gx PATH "$PATH:$HOME/.puro/bin" # Added by Puro
+set -gx PATH "$PATH:$HOME/.puro/shared/pub_cache/bin" # Added by Puro
+set -gx PATH "$PATH:$HOME/.puro/envs/default/flutter/bin" # Added by Puro
+set -gx PURO_ROOT "/home/zenmachi/.puro" # Added by Puro
+set -gx PATH "$PATH:$HOME/.cargo/bin"
 
 
 starship init fish | source
